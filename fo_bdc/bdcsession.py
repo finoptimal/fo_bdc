7ec842f39f7181b3ccc59b94d579e2b01c7a826a
Christopher Stamp <christopher@finoptimal.com>
2022-02-07 11:56:09 -0600

    Add bdc.log for requests calls to BDC.

--- fo_bdc/bdcsession.py
+++ fo_bdc/bdcsession.py
@@ -1,23 +1,34 @@
-# Python bindings for the Bill.com REST API, which is documented here:
-#  http://developer.bill.com/api-documentation/overview/
+"""
+Python bindings for the Bill.com REST API, which is documented here:
+http://developer.bill.com/api-documentation/overview/
 
-# Please contact developer@finoptimal.com with questions or comments.
+Please contact developer@finoptimal.com with questions or comments.
 
-from base64      import b64encode
+Copyright 2016-2022 FinOptimal, Inc. All rights reserved.
+"""
+import copy
+import json
+import os
+import requests
+from base64 import b64encode
 
-import copy, json, os, requests
+from finoptimal.logging import get_logger, get_file_logger, LoggedClass
 
-class BDCSession(object):
+logger = get_logger(__name__)
+bdc_logger = get_file_logger('bdc')
+
+
+class BDCSession(LoggedClass):
     """
     A session consitutes work on a single organization using a single set of
      login and api credentials.
     """
-    
+
     BASE_URL = "https://api.bill.com/api/v2"
-    HEADERS  = {'content-type': 'application/x-www-form-urlencoded'}
-    
+    HEADERS = {'content-type': 'application/x-www-form-urlencoded'}
+
     def __init__(self, username, password, api_key,
-                 organization_id=None, verbosity=0):    
+                 organization_id=None, verbosity=0):
         """
         You can't do much without an organization_id, but you can find out which
          organizations are available to you.
@@ -30,37 +41,37 @@ class BDCSession(object):
         self.ak = api_key
         self.oi = organization_id
         self.vb = verbosity
-        
+
         self._setup()
 
     def _setup(self):
-        self.si = self.session_id      = None
-        
+        self.si = self.session_id = None
+
         if not self.oi:
             orgs = self._call("ListOrgs", data=dict(
                 userName=self.un, password=self.pw))
             if len(orgs) == 1:
                 self.oi = self.oi = orgs[0]["orgId"]
-                print("{} ({}) is {}'s only organization, so".format(
-                    orgs[0]["orgName"], self.oi, self.un), "logging into that.")
+                self.print(f"{orgs[0]['orgName']} ({self.oi}) is {self.un}'s only organization, so",
+                           "logging into that.")
                 # Now that we've set oi...
                 self._setup()
             else:
-                print("{}'s available BDC Organizations:".format(self.un))
-                print(json.dumps(orgs, indent=4))
+                self.print(f"{self.un}'s available BDC Organizations:")
+                self.print(json.dumps(orgs, indent=4))
                 quit()
-                
+
         else:
             rd = self._call("Login", data=dict(
                 userName=self.un, password=self.pw, orgId=self.oi))
             self.si = self.session_id = rd["sessionId"]
 
             if self.vb > 7:
-                print("Successfully logged into orgId {}.".format(self.oi))
+                self.print(f"Successfully logged into orgId {self.oi}.")
 
         if not self.si:
             raise Exception("Not logged into BDC.")
-        
+
     def _call(self, url_tail, data=None, suppress_errors=False, **params):
         """
         This is a generic wrapper around the requests module, intended to take
@@ -73,36 +84,41 @@ class BDCSession(object):
             data = {}
 
         data["devKey"] = self.ak
-        
-        if not url_tail in ["Login", "ListOrgs"]:
+
+        if url_tail not in ["Login", "ListOrgs"]:
             # This goes in every call except these two excluded ones
             # For those two, more sensitive data is packed into the request
             #  body via the data parameter.
-            data["sessionId"] = self.si            
+            data["sessionId"] = self.si
 
-        # We just put all the params in the request body...
-        data.update({"data" : json.dumps(params)})
-            
-        full_url  = "{}/{}.json".format(self.BASE_URL, url_tail)
+            # We just put all the params in the request body...
+        data.update({"data": json.dumps(params)})
 
-        resp  = requests.post(full_url, headers=self.HEADERS, data=data)
+        full_url = f"{self.BASE_URL}/{url_tail}.json"
+
+        resp = requests.post(full_url, headers=self.HEADERS, data=data)
+
+        bdc_logger.info(f"{resp.__hash__()} - {resp.status_code} {resp.reason} - "
+                        f"{resp.request.method.ljust(4)} {resp.url}")
 
         # Response Json
         rj = resp.json()
 
         if self.vb > 7:
-            print(json.dumps(rj, indent=4))
+            self.print(json.dumps(rj, indent=4))
 
         if not rj["response_message"] == "Success":
             if not suppress_errors and self.vb > 0:
-                print(json.dumps(rj, indent=4))
-            if self.vb > 5:
-                print("Inspect full_url, data, rj:")
-                import ipdb;ipdb.set_trace()        
+                self.print(json.dumps(rj, indent=4))
 
-        # For troubleshooting...
+            if self.vb > 5:
+                self.print("Inspect full_url, data, rj:")
+                import ipdb
+                ipdb.set_trace()
+
+                # For troubleshooting...
         self.last_response = rj.copy()
-                
+
         # Response Data
         rd = rj["response_data"]
 
@@ -112,7 +128,7 @@ class BDCSession(object):
         resp = self._call("Logout")
 
         if self.vb > 7:
-            print("Successfully logged out of orgId {}.".format(self.oi))
+            self.print(f"Successfully logged out of orgId {self.oi}.")
 
     def _crud(self, operation, object_type, **params):
         """
@@ -122,7 +138,7 @@ class BDCSession(object):
 
         While operation is case-insensitive, object_type is NOT.
         """
-        url_tail = "Crud/{}/{}".format(operation.title(), object_type)
+        url_tail = f"Crud/{operation.title()}/{object_type}"
         return self._call(url_tail, **params)
 
     def create(self, object_type, obj=None, **params):
@@ -131,10 +147,10 @@ class BDCSession(object):
         """
         if not params.get("obj"):
             if obj:
-                params.update({"obj" : obj})
+                params.update({"obj": obj})
             else:
-                params = {"obj" : copy.deepcopy(params)}
-                
+                params = {"obj": copy.deepcopy(params)}
+
         return self._crud("Create", object_type, **params)
 
     def read(self, object_type, object_id):
@@ -150,17 +166,15 @@ class BDCSession(object):
         """
         if not params.get("obj"):
             if obj:
-                params = {"obj" : obj}
+                params = {"obj": obj}
             else:
-                params = {"obj" : params.copy()}
+                params = {"obj": params.copy()}
 
         object_id = params["obj"].get("id")
         if not object_id:
-            msg = "{}; params:{}".format(
-                "Need existing object id in order to update!",
-                json.dumps(params))
+            msg = f"Need existing object id in order to update!; params:{json.dumps(params)}"
             raise Exception(msg)
-        
+
         return self._crud("Update", object_type, id=object_id, **params)
 
     def delete(self, object_type, object_id):
@@ -169,8 +183,7 @@ class BDCSession(object):
     def undelete(self, object_type, object_id):
         return self._crud("Undelete", object_type, id=object_id)
 
-    def list(self, object_type, start=0, max=999, filters=None, sort=None,
-             suppress_errors=False):
+    def list(self, object_type, start=0, max=999, filters=None, sort=None, suppress_errors=False):
         """
         http://developer.bill.com/api-documentation/api/list
 
@@ -179,8 +192,11 @@ class BDCSession(object):
          emacs et al. Sorry...it seemed more important to mirror the endpoints.
          Hopefully there aren't downsides to the collision I'm not aware of.
         """
-        return self._call("List/{}".format(
-            object_type), start=start, max=max, filters=filters, sort=sort,
+        return self._call(f"List/{object_type}",
+                          start=start,
+                          max=max,
+                          filters=filters,
+                          sort=sort,
                           suppress_errors=suppress_errors)
 
     def current_time(self):
@@ -197,8 +213,8 @@ class BDCSession(object):
          https://developer.bill.com/hc/en-us/articles/218380103
         """
         params = {
-            "vendorId" : vendor_id,
-            "email"    : vendor_email
+            "vendorId": vendor_id,
+            "email": vendor_email
         }
 
         return self._call('SendVendorInvite', **params)
@@ -208,22 +224,22 @@ class BDCSession(object):
         https://developer.bill.com/hc/en-us/articles/213994086        
         """
         params = {
-            "id" : customer_or_vendor_id,
+            "id": customer_or_vendor_id,
         }
 
         return self._call('GetNetworkStatus', **params)
-    
+
     def attach_file(self, attachment_path, target_id=None, is_public=False):
         """
         If no target_id, document gets added to the Bill.com "Inbox".
         """
         data = {
-            "fileName" : os.path.split(attachment_path)[1],
-            "document" : b64encode(open(
+            "fileName": os.path.split(attachment_path)[1],
+            "document": b64encode(open(
                 attachment_path, "rb").read()).decode("utf-8"),
-            "isPublic" : is_public,
+            "isPublic": is_public,
         }
-        
+
         if target_id:
             data["id"] = target_id
 
@@ -236,18 +252,20 @@ class BDCSession(object):
         return self._call(
             "ClearApprovers", entity=object_type, objectId=object_id)
 
-    def list_user_approvals(self, user_id, object_type, approval_type,
-                            marker, max=999, nested=False):
+    def list_user_approvals(self, user_id, object_type, approval_type, marker, max=999, nested=False):
         """
         https://developer.bill.com/hc/en-us/articles/214115986-ListUserApprovals
 
         To Do: Implement sort, filters, and related...
         """
         return self._call("ListUserApprovals",
-                          usersId=user_id, entity=object_type,
-                          approvalType=approval_type, marker=marker,
-                          max=max, nested=nested)
-    
+                          usersId=user_id,
+                          entity=object_type,
+                          approvalType=approval_type,
+                          marker=marker,
+                          max=max,
+                          nested=nested)
+
     def set_approvers(self, object_type, object_id, user_ids):
         """
         https://developer.bill.com/hc/en-us/articles/210138853-SetApprovers
@@ -255,9 +273,10 @@ class BDCSession(object):
         user_ids should be a list; the first two params should be strings
         """
         return self._call("SetApprovers",
-                          entity=object_type, objectId=object_id,
+                          entity=object_type,
+                          objectId=object_id,
                           approvers=user_ids)
-    
+
     def record_ap_payment(self, obj=None, **params):
         """
         https://developer.bill.com/hc/en-us/articles/215407343-RecordAPPayment
@@ -266,7 +285,7 @@ class BDCSession(object):
             if len(params) > 0:
                 raise Exception("Don't provide both obj and params!")
             params = obj.copy()
-            
+
         return self._call("RecordAPPayment", **params)
 
     def record_ar_payment(self, obj=None, **params):
@@ -277,7 +296,7 @@ class BDCSession(object):
             if len(params) > 0:
                 raise Exception("Don't provide both obj and params!")
             params = obj.copy()
-            
+
         return self._call("RecordARPayment", **params)
 
     def set_customer_authorization(self, customer_id):
@@ -288,7 +307,7 @@ class BDCSession(object):
         return self._call("SetCustomerAuthorization",
                           customerId=customer_id,
                           hasAuthorizedToCharge=True)
-    
+
     def charge_customer(self, obj=None, **params):
         """
         WARNING -- THIS ACTUALLY MOVES MONEY
@@ -299,7 +318,7 @@ class BDCSession(object):
             if len(params) > 0:
                 raise Exception("Don't provide both obj and params!")
             params = obj.copy()
-            
+
         return self._call("ChargeCustomer", **params)
 
     def send_invoice(self, obj=None, **params):
@@ -312,14 +331,14 @@ class BDCSession(object):
             if len(params) > 0:
                 raise Exception("Don't provide both obj and params!")
             params = obj.copy()
-            
+
         return self._call("SendInvoice", **params)
 
     def get_entity_metadata(self, object_types):
         """
         https://developer.bill.com/hc/en-us/articles/210138323-GetEntityMetadata
         """
-        return self._call("GetEntityMetadata", entity=object_types) 
+        return self._call("GetEntityMetadata", entity=object_types)
 
     def get_disbursement_data(self, sent_pay_id):
         """
@@ -334,4 +353,6 @@ class BDCSession(object):
         """
         return self._call(
             "ListPayments",
-            disbursementStatus=disbursement_status, start=start, max=max)
+            disbursementStatus=disbursement_status,
+            start=start,
+            max=max)
